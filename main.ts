9b10cf71c45e5b9f3d9268dc3d08ccb247166904
liuxingyu521 <xuj_power@qq.com>
2023-11-14 22:27:59 +0800

    fix: add registerEvent to unload plugin correctly

--- main.ts
+++ main.ts
@@ -5,12 +5,15 @@ const DEFAULT_LINE_NUM = 999;
 
 export default class CursorGoaway extends Plugin {
 	async onload() {
-		this.app.workspace.on('file-open', (file) => {
-			if (file?.extension === 'md') {
-				const lastLineNum = this.app.workspace.activeEditor?.editor?.lineCount() || DEFAULT_LINE_NUM;
-				const cursorPosition = this.app.workspace.activeEditor?.editor?.getCursor();
-				this.app.workspace.activeEditor?.editor?.setCursor(cursorPosition?.line ? cursorPosition : lastLineNum)
-			}
-		})
+		this.registerEvent(
+			this.app.workspace.on('file-open', (file) => {
+				if (file?.extension === 'md') {
+					const lastLineNum = this.app.workspace.activeEditor?.editor?.lineCount() || DEFAULT_LINE_NUM;
+					const cursorPosition = this.app.workspace.activeEditor?.editor?.getCursor();
+					this.app.workspace.activeEditor?.editor?.setCursor(cursorPosition?.line ? cursorPosition : lastLineNum)
+				}
+			})
+		)
+		
 	}
 }
