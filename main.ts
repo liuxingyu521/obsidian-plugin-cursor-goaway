958da23780c8466d1201354920ab95f6c4523bf1
liuxingyu521 <xuj_power@qq.com>
2024-01-30 14:48:37 +0800

    fix: change the way of cursor goaway

--- main.ts
+++ main.ts
@@ -1,16 +1,12 @@
 import { Plugin } from 'obsidian';
 
-/** 打开文件光标默认行数 */
-const DEFAULT_LINE_NUM = 999;
-
 export default class CursorGoaway extends Plugin {
 	async onload() {
 		this.registerEvent(
 			this.app.workspace.on('file-open', (file) => {
 				if (file?.extension === 'md') {
-					const lastLineNum = this.app.workspace.activeEditor?.editor?.lineCount() || DEFAULT_LINE_NUM;
-					const cursorPosition = this.app.workspace.activeEditor?.editor?.getCursor();
-					this.app.workspace.activeEditor?.editor?.setCursor(cursorPosition?.line ? cursorPosition : lastLineNum)
+					// when toggle to the md file, blur the page to avoid break the preview style
+					this.app.workspace.activeEditor?.editor?.blur();
 				}
 			})
 		)
