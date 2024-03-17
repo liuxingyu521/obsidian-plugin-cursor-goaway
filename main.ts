155f62ed65cade7cee10cb2ece7194ebfceb044d
liuxingyu521 <xuj_power@qq.com>
2024-03-17 21:58:50 +0800

    fix: 打开文件偶现光标的问题,使用 requestAnimationFrame 重构事件执行

--- main.ts
+++ main.ts
@@ -4,9 +4,20 @@ export default class CursorGoaway extends Plugin {
 	async onload() {
 		this.registerEvent(
 			this.app.workspace.on('file-open', (file) => {
-				if (file?.extension === 'md') {
-					// when toggle to the md file, blur the page to avoid break the preview style
+				let startTime: number;
+				
+				const blurEditor = () => {
 					this.app.workspace.activeEditor?.editor?.blur();
+					
+					if (performance.now() - startTime < 500) {
+						requestAnimationFrame(blurEditor)
+					}
+				}
+
+				// when toggle to the md file, blur the page in 500ms to avoid break the preview style	
+				if (file?.extension === 'md') {
+					startTime = performance.now();
+					requestAnimationFrame(blurEditor);
 				}
 			})
 		)
